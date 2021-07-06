dc46c974621122bfaf0ace0df114c6e2ef179100
Leon <salient@Full-Stack-SEO.local>
2021-07-06 18:18:57 -0400

    added kernal reset functionality

--- nimnotebook.nim
+++ nimnotebook.nim
@@ -2,13 +2,13 @@
 
 import neelmodified, osproc, os, threadpool, streams, strutils
 
-var p = startProcess(command="nim",args=["secret"], options = {poUsePath})
+var p = startProcess(command="nim", args=["secret"], options = {poUsePath}) #can't use stderror option wtf
 var inp = inputStream(p)
 var outp = outputStream(p)
 
 exposeProcs:
-  proc kernal(id,script: string) =
 
+  proc kernal(id,script: string) =
       inp.write(script & "\necho(\"ENDOFSCRIPT\")\n")
       inp.flush()
 
@@ -21,5 +21,16 @@ exposeProcs:
                   output.add(line & "\n")
 
       callJs("kernalOutput",id, output)
+
+  proc resetKernal() =
+        p.terminate()
+        p.close()
+        echo "Kernal killed"
+        p = startProcess(command="nim", args=["secret"], options = {poUsePath})
+        inp = inputStream(p)
+        outp = outputStream(p)
+        echo "New kernal started"
+        callJs("logMe","kernal was killed, then reset")
+
 
 startApp(appMode=false)
